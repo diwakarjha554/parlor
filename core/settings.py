ddfb36dc923a4ba656071f1b73cc329b0bb875f4
Sachin sharma <indegamer1@gmail.com>
2024-07-03 18:02:28 +0100

    Home Page Route Setup

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Core_Application',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
